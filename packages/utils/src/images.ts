e35688f272446d98149e98342db36695c36c9a37
Knav <me@knav.link>
2022-01-30 10:06:37 -0800

    Makes image service uri configurable

--- packages/utils/src/images.ts
+++ packages/utils/src/images.ts
@@ -1,6 +1,14 @@
 import { HiddenTraits, TraitType } from '@chain-runners/types'
 import { ALL_TRAIT_ORDERING } from './traits'
 
+const IMAGE_API_CONFIG: { baseURI: string } = {
+  baseURI: 'https://img.chainrunners.xyz',
+}
+
+export function setImageServiceBaseURI(baseURI: string) {
+  IMAGE_API_CONFIG.baseURI = baseURI
+}
+
 export function encodeLayerFlags(hiddenTraits: HiddenTraits): string | undefined {
   const flags: Array<number> = Object.values(TraitType)
     .filter((traitType: TraitType) => hiddenTraits[traitType])
@@ -24,12 +32,12 @@ export function getImageUrlForToken(
   const layerFlags = hiddenTraits ? encodeLayerFlags(hiddenTraits) : undefined
   const params = layerFlags ? `?layerFlags=${layerFlags}` : ''
 
-  return `https://img.chainrunners.xyz/api/v1/tokens/${fileType}/${tokenId}${params}`
+  return `${IMAGE_API_CONFIG.baseURI}/api/v1/tokens/${fileType}/${tokenId}${params}`
 }
 
 export function getImageUrlForTrait(
   traitId: number,
   fileType: ImageFileType = ImageFileType.png,
 ): string {
-  return `https://img.chainrunners.xyz/api/v1/traits/${fileType}/${traitId}.${fileType}`
+  return `${IMAGE_API_CONFIG.baseURI}/api/v1/traits/${fileType}/${traitId}.${fileType}`
 }
