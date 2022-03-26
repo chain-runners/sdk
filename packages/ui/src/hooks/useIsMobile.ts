9fca83c3c698372e60876678b5d59f84aacea33c
Knav <me@knav.link>
2022-03-26 06:22:18 -0700

    Updates isMobile width

--- packages/ui/src/hooks/useIsMobile.ts
+++ packages/ui/src/hooks/useIsMobile.ts
@@ -1,7 +1,18 @@
 import { useMemo } from 'react'
 import { useWindowSize } from './useWindowSize'
 
-export function useIsMobile(): boolean {
+const DEFAULT_CONFIG = {
+  threshold: 900,
+}
+
+export function setDefaultIsMobileConfig(threshold: number) {
+  DEFAULT_CONFIG.threshold = threshold
+}
+
+export function useIsMobile(threshold?: number): boolean {
   const { width: pageWidth } = useWindowSize()
-  return useMemo(() => pageWidth <= 530, [pageWidth])
+  return useMemo(
+    () => pageWidth <= (threshold ?? DEFAULT_CONFIG.threshold),
+    [pageWidth, threshold],
+  )
 }
