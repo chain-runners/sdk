448721747e398025bab30c15cc3f1fdf7cef4972
Knav <me@knav.link>
2022-07-11 15:55:37 -0700

    explicit type

--- packages/integration-tests/src/traitParsing.ts
+++ packages/integration-tests/src/traitParsing.ts
@@ -1,5 +1,6 @@
 import { getChainRunnersAPIClient } from '@chain-runners/api-client'
 import { ALL_TRAITS } from '@chain-runners/data'
+import { RunnerTrait } from '@chain-runners/types'
 import { getSelectedTraits } from '@chain-runners/utils'
 import { getOrThrow } from '@chain-runners/utils/dist/types'
 
@@ -11,7 +12,7 @@ async function main() {
   console.log(token.dnaString)
 
   const traits = getSelectedTraits(ALL_TRAITS, token.dnaString)
-  console.log(traits.map((t) => t.displayName))
+  console.log(traits.map((t: RunnerTrait) => t.displayName))
 }
 
 main().then(() => {
