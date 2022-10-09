68f5e9588bd6535af60008debe7b22af9d919eae
Knav <me@knav.link>
2022-10-09 11:37:27 -0700

    Export PrimaryColorProviders

--- packages/ui/src/hooks/index.ts
+++ packages/ui/src/hooks/index.ts
@@ -1,4 +1,8 @@
 export { usePersistedState } from './usePersistedState'
-export { usePrimaryColor } from './usePrimaryColor'
+export {
+  usePrimaryColor,
+  PersistentPrimaryThemeColorProvider,
+  PrimaryThemeColorProvider,
+} from './usePrimaryColor'
 export { useIsMobile } from './useIsMobile'
 export { useWindowSize } from './useWindowSize'
